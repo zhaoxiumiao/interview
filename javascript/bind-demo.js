06f3e525b6b49aa679573d61f89798d4cb46df1a
zhao xiu miao <1027583934@qq.com>
2021-04-15 23:26:52 +0800

    “async”

--- javascript/bind-demo.js
+++ javascript/bind-demo.js
@@ -1,9 +1,35 @@
+// function fn1 (a,b){
+//     console.log('this',this);
+//     console.log(a,b);
+//     return 'this is fn1'
+// }
+
+// const fn2 = fn1.bind({x:100},10,20,30)
+// const res = fn2()
+// console.log(res);
+
+//模拟 bind
+Function.prototype.bind1 = function(){
+    // 将参数拆解为数组
+    const args = Array.prototype.slice.call(arguments)
+
+    //获取 this (数组第一项)
+    const t = args.shift()
+
+    //fn1.bind(...) 中的 fn1
+    const self = this
+    
+    return function(){
+        return self.apply(t,args)
+    }
+}
+
 function fn1 (a,b){
     console.log('this',this);
     console.log(a,b);
     return 'this is fn1'
 }
 
-const fn2 = fn1.bind({x:100},10,20,30)
+const fn2 = fn1.bind1({x:100},10,20,30)
 const res = fn2()
 console.log(res);
